8759c300a3fde56fe0b27dbb6a311bd4f2e39e91
shubhankarsharan <shubhankarsharan@gmail.com>
2021-07-21 22:50:09 -0700

    finish tutorial (no router because using next)

--- pages/components/Popular.js
+++ pages/components/Popular.js
@@ -1,14 +1,18 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { fetchPopularRepos } from '../api/api.js'
+import { fetchPopularRepos } from '../api/api'
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
+import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
   return (
     <ul className='flex-center'>
-      {languages.map((language, index) => (
-        <li key={index}>
+      {languages.map((language) => (
+        <li key={language}>
           <button
             className='btn-clear nav-link'
             style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
@@ -26,6 +30,55 @@ LangaugesNav.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired
 }
 
+function ReposGrid ({ repos }) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+        const { login, avatar_url } = owner
+
+        return (
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <Tooltip text="Github Username">
+                    <FaUser color='rgb(255, 191, 116)' size={22} />
+                    <a href={`https://github.com/${login}`}>
+                      {login}
+                    </a>
+                  </Tooltip>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props)
@@ -39,11 +92,9 @@ export default class Popular extends React.Component {
     this.updateLanguage = this.updateLanguage.bind(this)
     this.isLoading = this.isLoading.bind(this)
   }
-
   componentDidMount () {
     this.updateLanguage(this.state.selectedLanguage)
   }
-
   updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage,
@@ -56,7 +107,7 @@ export default class Popular extends React.Component {
           this.setState(({ repos }) => ({
             repos: {
               ...repos,
-              [selectedLanguage] : data
+              [selectedLanguage]: data
             }
           }))
         })
@@ -69,13 +120,11 @@ export default class Popular extends React.Component {
         })
     }
   }
-
   isLoading() {
     const { selectedLanguage, repos, error } = this.state
 
-    return ! repos[selectedLanguage] && error === null
+    return !repos[selectedLanguage] && error === null
   }
-
   render() {
     const { selectedLanguage, repos, error } = this.state
 
@@ -86,11 +135,11 @@ export default class Popular extends React.Component {
           onUpdateLanguage={this.updateLanguage}
         />
 
-        {this.isLoading() && <p>LOADING</p>}
+        {this.isLoading() && <Loading text='Fetching Repos' />}
 
-        {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
 
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
+import React from 'react'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Navbar from './components/Navbar'
 
-export default function Home() {
-  return (
-    <Popular />
-  )
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+
+  render () {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Navbar />
+            <Popular />
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
